d887767770d9d6232f1cb40419b8fc06aacfea31
Yas <gabilondoyasmin@gmail.com>
2023-07-14 01:44:29 -0300

    Se crea una funcion que verifique si el dato ingresado es un str. A parte, imprime la cantidad de vidas restantes

--- AdivinaLaPalabra.py
+++ AdivinaLaPalabra.py
@@ -1,11 +1,11 @@
-print("---INICIO DEL JUEGO---")
+print("\n---INICIO DEL JUEGO---")
 print("Vamos a jugar al ahorcado!")
 #resp=input("Quieres jugar?")
 
 from random import choice 
-palabras=["gato","perro","pajaro"]
+palabras=["gato","perro","pajaro","raton","iguana"]
 fallidas=[]
-acertadas=["p","a"]
+acertadas=[]
 vidas=5
 juego_terminado=False
 
@@ -22,8 +22,28 @@ def guiones(palabra_oculta):
             lista_ocult.append(l)
         else:
             lista_ocult.append("-")
+            vidas=-1
     print(lista_ocult)
+
+def verificacion(palabra_oculta,l):
+    if type(l)==str:
+        if l in palabra_oculta:
+            acertadas.append(l)
+            cont=+1
+        else:
+            fallidas.append(l)
+    else:
+        print("La letra ingresada no es valida")
+        l=print("Ingresa una nueva letra: ")
+        verificacion(l)
+    return palabra_oculta
 
 palabra_oculta = eleccion(palabras) #Asignamos el resultadoa una nueva variable
 letras_correctas=len(set(palabra_oculta)) #Contamos cuantas letras correctas tiene la palabra oculta
+print("================================")
+print(f"La paralabra que debes adivinar tiene",letras_correctas,"letras correctas")
+l=input("Que letra quieres probar? ")
+verificacion(palabra_oculta,l)
 guiones(palabra_oculta)
+print(f"Te restan",vidas,"vidas")
+print(f"Tus letras incorrectas han sido: ",fallidas)
