5d90fe2a7757b31bcbb567ed469bdc534b056fa7
Yas <gabilondoyasmin@gmail.com>
2023-07-15 00:24:44 -0300

    Se agrega el bucle, dando comienzo al juego, y las condicionales que determinan el cierre del mismo

--- AdivinaLaPalabra.py
+++ AdivinaLaPalabra.py
@@ -1,13 +1,13 @@
 print("\n---INICIO DEL JUEGO---")
 print("Vamos a jugar al ahorcado!")
-#resp=input("Quieres jugar?")
 
+#Se crean las variables iniciales
 from random import choice 
-palabras=["gato","aperro","pajaro","araton","iguana"]
+palabras=["gato","perro","pajaro","raton","iguana"]
 fallidas=[]
 acertadas=[]
 vidas=5
-juego_terminado=False
+cont=0
 
 #Eleccion de palabra a adivinar
 def eleccion(palabras):
@@ -18,34 +18,54 @@ def eleccion(palabras):
 def guiones(palabra_oculta,l):
     lista_ocult=[]
     for l in palabra_oculta:
-        if l in acertadas:
-            lista_ocult.append(l)
+        if l.lower() in acertadas:
+            lista_ocult.append(l.lower())
         else:
             lista_ocult.append("-")
     print(lista_ocult)
 
 #Verifica si la letra ingresada es del tipo string, y si se encuentra en la palabra oculta
-def verificacion(palabra_oculta,l,vidas):
-    cont=0
+#Resta vidas si es incorrecta, o suma un contador de aciertos si es correcta
+def verificacion(palabra_oculta,l,vidas,cont):
     if type(l)==str:
-        if l in palabra_oculta:
-            acertadas.append(l)
-            cont+=1
-            print(f"Te restan",vidas,"vidas")
+        if l.lower() in palabra_oculta:
+            if l.lower() not in acertadas:
+                print("======================")
+                acertadas.append(l.lower())
+                print(f"Te restan",vidas,"vidas")
+                cont+=1
         else:
-            fallidas.append(l)
+            print("======================")
+            print("Fallaste D:")
+            fallidas.append(l.lower())
             vidas-=1
             print(f"Te restan",vidas,"vidas")
     else:
         print("La letra ingresada no es valida")
         l=input("Ingresa una nueva letra: ")
-    return l, cont
+    return l, cont, vidas
 
 palabra_oculta = eleccion(palabras) #Asignamos el resultadoa una nueva variable
 letras_correctas=len(set(palabra_oculta)) #Contamos cuantas letras correctas tiene la palabra oculta
-print("================================")
-print(f"La paralabra que debes adivinar tiene",letras_correctas,"letras correctas")
-l=input("Que letra quieres probar? ")
-l,cont=verificacion(palabra_oculta,l,vidas)
-guiones(palabra_oculta,l)
-print(f"Tus letras incorrectas han sido: ",fallidas)
+
+#Comienza el bucle del juego, se pide la letra al usuario, y se llaman a las funciones
+while vidas>0: 
+    print("===============================")
+    print(f"La paralabra que debes adivinar tiene",letras_correctas,"letras correctas")
+    l=input("Que letra quieres probar? ")
+    l, cont, vidas=verificacion(palabra_oculta,l,vidas,cont)
+    guiones(palabra_oculta,l)
+    print(f"Tus letras incorrectas han sido: ",fallidas)
+
+    #A continuacion, las condicionales determinan el restulado del juego y cierran el bucle
+    if cont>=letras_correctas:
+        print("===============================")
+        print("Ganaste!!!")
+        print(f"Tu palabra oculta es:",palabra_oculta.upper())
+        print("Gracias por jugar!!")
+        break
+    if vidas==0:
+        print("===============================")
+        print("Has fallado D:")
+        print("Vuelve a intentarlo pronto!")
+        break
