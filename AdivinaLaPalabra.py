307ecd879392175de491787fc752e5f0c4ada22a
Yas <gabilondoyasmin@gmail.com>
2023-07-14 22:40:40 -0300

    Se agrega un contador de aciertos que suma si la letra es correcta, y si no es correcta, se resta una vida

--- AdivinaLaPalabra.py
+++ AdivinaLaPalabra.py
@@ -3,7 +3,7 @@ print("Vamos a jugar al ahorcado!")
 #resp=input("Quieres jugar?")
 
 from random import choice 
-palabras=["gato","perro","pajaro","raton","iguana"]
+palabras=["gato","aperro","pajaro","araton","iguana"]
 fallidas=[]
 acertadas=[]
 vidas=5
@@ -15,35 +15,37 @@ def eleccion(palabras):
     return palabra_oculta
 
 #Muestra la letra seleccionada si es correcta o un guion si es incorrecta
-def guiones(palabra_oculta):
+def guiones(palabra_oculta,l):
     lista_ocult=[]
     for l in palabra_oculta:
         if l in acertadas:
             lista_ocult.append(l)
         else:
             lista_ocult.append("-")
-            vidas=-1
     print(lista_ocult)
 
-def verificacion(palabra_oculta,l):
+#Verifica si la letra ingresada es del tipo string, y si se encuentra en la palabra oculta
+def verificacion(palabra_oculta,l,vidas):
+    cont=0
     if type(l)==str:
         if l in palabra_oculta:
             acertadas.append(l)
-            cont=+1
+            cont+=1
+            print(f"Te restan",vidas,"vidas")
         else:
             fallidas.append(l)
+            vidas-=1
+            print(f"Te restan",vidas,"vidas")
     else:
         print("La letra ingresada no es valida")
-        l=print("Ingresa una nueva letra: ")
-        verificacion(l)
-    return palabra_oculta
+        l=input("Ingresa una nueva letra: ")
+    return l, cont
 
 palabra_oculta = eleccion(palabras) #Asignamos el resultadoa una nueva variable
 letras_correctas=len(set(palabra_oculta)) #Contamos cuantas letras correctas tiene la palabra oculta
 print("================================")
 print(f"La paralabra que debes adivinar tiene",letras_correctas,"letras correctas")
 l=input("Que letra quieres probar? ")
-verificacion(palabra_oculta,l)
-guiones(palabra_oculta)
-print(f"Te restan",vidas,"vidas")
+l,cont=verificacion(palabra_oculta,l,vidas)
+guiones(palabra_oculta,l)
 print(f"Tus letras incorrectas han sido: ",fallidas)
